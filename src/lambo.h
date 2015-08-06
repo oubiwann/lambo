21ef53bad7e814fdfa92c1777237b3ee9123f7b1
Duncan McGreggor <oubiwann@gmail.com>
2015-08-05 21:32:03 -0500

    Added fake funcs for Windows.

--- src/lambo.h
+++ src/lambo.h
@@ -5,7 +5,24 @@
 #include <editline/history.h>
 #endif
 
+#ifdef _WIN32
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+#else
 #include <editline/readline.h>
+#endif
 
 // add def for project title
 // add def for project name
