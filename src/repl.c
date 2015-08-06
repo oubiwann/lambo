ca850a7f87fa545bcefb09bd1e465a540127b883
Duncan McGreggor <oubiwann@gmail.com>
2015-08-05 22:27:17 -0500

    Added eval.

--- src/repl.c
+++ src/repl.c
@@ -8,11 +8,46 @@ void show_banner () {
   puts("Press Ctrl+c to Exit\n");
 }
 
-void parse_input (char* input,mpc_parser_t* Lambo) {
+
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+
+long eval(mpc_ast_t* t) {
+
+  /* If tagged as number return it directly. */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child. */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+
+void parse_input (char* input, mpc_parser_t* Lambo) {
   mpc_result_t r;
   if (mpc_parse("<stdin>", input, Lambo, &r)) {
-    /* On success print and delete the AST */
-    mpc_ast_print(r.output);
+    long result = eval(r.output);
+    printf("%li\n", result);
     mpc_ast_delete(r.output);
   } else {
     /* Otherwise print and delete the Error */
@@ -20,6 +55,7 @@ void parse_input (char* input,mpc_parser_t* Lambo) {
     mpc_err_delete(r.error);
   }
 }
+
 
 int main(int argc, char** argv) {
 
@@ -30,7 +66,7 @@ int main(int argc, char** argv) {
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr     = mpc_new("expr");
   mpc_parser_t* Lambo    = mpc_new("lambo");
-  
+
   /* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
@@ -40,13 +76,13 @@ int main(int argc, char** argv) {
       lambo    : /^/ <operator> <expr>+ /$/ ;             \
     ", Number, Operator, Expr, Lambo);
 
-  
+
   /* In a never ending loop */
   while (1) {
 
     /* Output our prompt and get input */
     char* input = readline("lambo> ");
-    
+
     /* Add input to history */
     add_history(input);
 
