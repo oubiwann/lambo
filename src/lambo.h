4ffe72eba7766f09edbdcb5a228ca901244c268e
Duncan McGreggor <oubiwann@gmail.com>
2015-08-05 21:28:08 -0500

    Moved includes into file and added readline.

---
+++ src/lambo.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#if defined(__linux__)
+#include <editline/history.h>
+#endif
+
+#include <editline/readline.h>
+
+// add def for project title
+// add def for project name
--- src/repl.c
+++ src/repl.c
@@ -1,25 +1,29 @@
-#include <stdio.h>
+#include "lambo.h"
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
-
-int main(int argc, char** argv) {
-
+void show_banner () {
   /* Print Version and Exit Information */
   puts("LAMBO Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
+}
+
+int main(int argc, char** argv) {
+
+  show_banner();
 
   /* In a never ending loop */
   while (1) {
 
-    /* Output our prompt */
-    fputs("lambo> ", stdout);
+    /* Output our prompt and get input */
+    char* input = readline("lambo> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Add input to history */
+    add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
   }
 
   return 0;
